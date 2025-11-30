9381a4b4757a51ca23da9e57b5c8a5ac3cc3caec
suhail sayyed <suhailsayyed01@gmail.com>
2025-11-30 21:20:30 +0530

    updated 3 files with minor projects

--- matrix text/script.js
+++ matrix text/script.js
@@ -6,14 +6,17 @@ abcdefghijklmnopqrstuvwxyz
 `.replace(/\s+/g, ""); // removes spaces & newlines → clean character list
 
 const text = para.innerHTML;
-
+let itration = 0;
 para.addEventListener("mouseenter", () => {
     setInterval(() => {
         const str = text.split("").map((char, index) => {
+            if(index < itration){
+                return char
+            }
             return characters[Math.floor(Math.random() * characters.length)];
         }).join("");
-
         para.innerHTML = str; 
+        itration += 0.2;
     }, 30);
 });
 
